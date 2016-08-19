e1a67e01d9a15c2b31c98ab07632127fa298e4eb
久乔 <ljx104734@alibaba-inc.com>
2016-08-19 10:00:56 +0800

    增加测试代码

--- www/components/popup/popup.js
+++ www/components/popup/popup.js
@@ -1,5 +1,51 @@
 define(['css!./popup','tpl!./popup.tpl',"jquery"], function(css,popuptpl,$) {
 	
+
+
+	var scriptArray=[];
+	for(var i=0;i<10;i++){
+
+		var a=function(){
+			
+			var mod=new module(arguments.callee.module_i);
+			scriptArray.push(mod);
+		};
+		a.module_i=i;
+		new a();
+		// (function(module_i){
+		// 	var mod=new module(module_i);
+		// 	scriptArray.push(mod);
+		// })(i)
+	}
+	function module(i){
+		this.name="name"+i;
+		this.callBack=function(){
+			console.log(this.name);
+		}
+	}
+
+
+	var timerid=null
+	var i=0;
+	function run(){
+		if(timerid){
+			window.clearTimeout(timerid);
+		}
+		i++;
+		if(i<10){
+			console.log(i);
+			scriptArray[i].callBack();
+			window.setTimeout(function(){
+				run();
+			},1000);
+		}
+
+
+	} 
+
+	run();
+
+
  
   return {
   		getHtml:function(){
